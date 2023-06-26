6497e0d026b270c6bc18791726ef94df1333a1dd
Rohan23459 <102240016+Rohan23459@users.noreply.github.com>
2023-06-26 15:58:58 +0530

    created solver cpp file

---
+++ solver.cpp
@@ -0,0 +1,15 @@
+/*
+Created By:- Rohan Kumar
+Date: 19 June 2021
+
+Implementation of backtracking algorithm and other game-play mechanics
+*/
+
+#include "solver.hpp"
+#include <iostream>
+#include <cassert>
+#include <math.h>
+#include <cstdlib>
+#include <ctime>
+
+using namespace std;
