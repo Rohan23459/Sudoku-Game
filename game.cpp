c879f1e32707a8a22550829bb35070d7cbc94b61
Rohan23459 <102240016+Rohan23459@users.noreply.github.com>
2023-06-26 15:52:37 +0530

    created game file

---
+++ game.cpp
@@ -0,0 +1,17 @@
+/*
+Created By:- Rohan Kumar
+Date: 22 June 2021
+
+File to implement game play
+*/
+
+#include <iostream>
+#include <regex>
+#include <sstream>
+#include "game.hpp"
+#include "solver.hpp"
+#include "altproj.hpp"
+using namespace std;
+
+void playGame(int size, int nobs);
+
