1282ca985b1431a69c6ed93a02af2c513a313ecd
Rohan23459 <102240016+Rohan23459@users.noreply.github.com>
2023-06-26 17:03:01 +0530

    Implement Driver function

--- main.cpp
+++ main.cpp
@@ -13,3 +13,126 @@ Main driver of Sudoku game
 #include "tests.hpp"
 
 using namespace std;
+
+
+int main(int argc, char *argv[])
+{
+    int gamesize, nobs;
+    // Welcome message
+    cout << "===========================================" << endl;
+    cout << "============ Welcome to Sudoku! ===========" << endl;
+    cout << "===========================================\n\n"
+         << endl;
+
+    cout << "Please enter the box size: ";
+    cin >> gamesize;
+    cout << "How many cell do you want to prefilled: ";
+    cin >> nobs;
+
+    int seed;                    // random seed
+    int sim = 0;                 // number of simulation runs (if running speed tests)
+    bool verbose = false;        // print each simulation result for speed tests
+    string method = "backtrace"; // choose solving method
+
+    // Handle command line args
+    for (int i = 1; i < argc; ++i)
+    {
+        if ((std::string(argv[i]) == "--seed") ||
+            (std::string(argv[i]) == "-s"))
+        {
+
+            if (i + 1 < argc)
+            {
+                istringstream ss(argv[++i]);
+                if (!(ss >> seed))
+                    cerr << "Invalid number " << argv[i++] << endl;
+            }
+            else
+            {
+                std::cerr << "--seed option requires one argument."
+                          << std::endl;
+                return 1;
+            }
+        }
+
+        if ((std::string(argv[i]) == "--gamesize") ||
+            (std::string(argv[i]) == "-g"))
+        {
+
+            if (i + 1 < argc)
+            {
+                istringstream ss(argv[++i]);
+                if (!(ss >> gamesize))
+                    cerr << "Invalid number " << argv[i++] << endl;
+            }
+            else
+            {
+                std::cerr << "--gamesize option requires one argument."
+                          << std::endl;
+                return 1;
+            }
+        }
+
+        if ((std::string(argv[i]) == "--nobs") ||
+            (std::string(argv[i]) == "-n"))
+        {
+
+            if (i + 1 < argc)
+            {
+                istringstream ss(argv[++i]);
+                if (!(ss >> nobs))
+                    cerr << "Invalid number " << argv[i++] << endl;
+            }
+            else
+            {
+                std::cerr << "--nobs option requires one argument."
+                          << std::endl;
+                return 1;
+            }
+        }
+
+        if ((std::string(argv[i]) == "--Unittest") ||
+            (std::string(argv[i]) == "-u"))
+        {
+
+            if (i + 1 < argc)
+            {
+                istringstream ss(argv[++i]);
+                if (!(ss >> sim))
+                    cerr << "Invalid number " << argv[i++] << endl;
+            }
+            else
+            {
+                std::cerr << "--Unittest option requires one argument."
+                          << std::endl;
+                return 1;
+            }
+        }
+
+        if ((std::string(argv[i]) == "--Verbose") ||
+            (std::string(argv[i]) == "-v"))
+        {
+            verbose = true;
+        }
+
+        if ((std::string(argv[i]) == "--RP") ||
+            (std::string(argv[i]) == "-rp"))
+        {
+            method = "RP";
+        }
+    }
+
+    srand(seed); // set random seed
+
+    // Play game or run simulations
+    if (sim == 0)
+    {
+        playGame(gamesize, nobs);
+    }
+    else
+    {
+        unitTest(gamesize, nobs, sim, verbose);
+    }
+
+    return 0;
+}
