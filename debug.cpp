00eec8cfa9c16d436fc26e46ce2dfe6bea787362
Rohan23459 <102240016+Rohan23459@users.noreply.github.com>
2023-06-26 16:50:20 +0530

    created debug function

---
+++ debug.cpp
@@ -0,0 +1,13 @@
+/*
+Created By:- Rohan Kumar
+Date: 16 June 2021
+*/
+
+
+#include <iostream>
+#include <cstdlib>
+#include<ctime>
+#include "solver.hpp"
+#include "altproj.hpp"
+using namespace std;
+using namespace arma;
